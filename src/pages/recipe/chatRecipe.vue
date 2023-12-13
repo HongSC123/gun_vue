3fa6e72167e3dee314a0abc1d1cc5252b0f2583b
JS Kim <kiqbbq0602@gmail.com>
2023-12-13 10:48:40 +0900

    백업용

--- src/pages/recipe/chatRecipe.vue
+++ src/pages/recipe/chatRecipe.vue
@@ -1,53 +1,30 @@
 <template>
-  <VContainer>
-    <VRow justify="center">
-      <VBtn
-        color="primary"
-        variant="tonal"
-        :to="{path: '/'}"
-        class="mr-3"
-      >
-        메인화면으로
-        <VIcon
-          end
-          icon="mdi-home"
-        />
-      </VBtn>
-      <VBtn
-        color="secondary"
-        variant="tonal"
-        :to="{path: 'chatrecent'}"
-        class="mr-3"
-      >
-        최근 답변
-        <VIcon
-          end
-          icon="mdi-history"
-        />
-      </VBtn>
-      <VBtn
-        color="info"
-        variant="tonal"
-        :to="{path: 'chatfixed'}"
-        class="mr-3"
-      >
-        고정 답변
-        <VIcon
-          end
-          icon="mdi-heart"
-        />
-      </VBtn>
-    </VRow>
-  </VContainer>
-  <div id="chat-container">
-    <div id="chat-messages">
-      <!-- 메시지를 표시할 영역 -->
-      <div
-        v-for="(msg, index) in chatHistory"
-        :key="index"
-        :class="msg.isUserMessage ? 'user-message' : 'ai-message'"
-      >
-        {{ msg.sender }} : {{ msg.message }}
+
+      <v-container>
+     <v-row justify="center">
+            <VBtn color="primary" variant="tonal" :to="{path: '/'}" class="mr-3">
+              메인화면으로
+              <VIcon end icon="mdi-home"/>
+            </VBtn>
+            <VBtn color="secondary" variant="tonal" :to="{path: 'chatrecent'}" class="mr-3">
+              최근 답변
+              <VIcon end icon="mdi-history"/>
+            </VBtn>
+            <VBtn color="info" variant="tonal" :to="{path: 'chatfixed'}" class="mr-3">
+              고정 답변
+              <VIcon end icon="mdi-heart"/>
+            </VBtn>
+          </v-row>
+        </v-container>
+    <div id="chat-container">
+      <div id="chat-messages">
+        <div v-for="(msg, index) in chatHistory" :key="index" :class="msg.isUserMessage ? 'user-message' : 'ai-message'">
+          {{ msg.sender }} : {{ msg.message }}
+        </div>
+      </div>
+      <div id="user-input">
+        <input type="text" placeholder="레시피를 검색하고 싶은 요리 이름을 적어주세요!" v-model="userMessage" />
+        <button @click="sendMessage">전송</button>
       </div>
     </div>
     <div id="user-input">
@@ -65,56 +42,45 @@
   
     
 <script>
-import axios from '@axios'
+import axios from '@axios';
   
-export default {
-  data() {
-    return {
-      userMessage: '', // 사용자 입력 메시지를 담을 데이터 속성
-      chatHistory: [], // 채팅 기록을 담을 배열
-      apiKey: "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",
-      apiEndpoint: 'https://api.openai.com/v1/chat/completions',
-    }
-  },
-  mounted() {
-    // 초기 화면에 인삿말 추가
-    this.addMessage('라따뚜AI', '안녕하세요! 어떤 레시피를 검색하고 싶으세요? 음식 이름만 말해주시면 조리법과 1인분 열량을 알려드리겠습니다!')
 
-    // Enter 키 이벤트를 추가합니다.
-    document.addEventListener('keydown', this.handleKeyDown)
-  },
-  beforeUnmount() {
-    // 컴포넌트가 삭제되기 전에 이벤트 리스너를 해제합니다.
-    document.removeEventListener('keydown', this.handleKeyDown)
-  },
-  methods: {
-    handleKeyDown(event) {
-      if (event.key === 'Enter') {
-        this.sendMessage()
-      }
+  export default {
+    data() {
+      return {
+        userMessage: '',
+        chatHistory: [], 
+        apiKey: "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",
+        apiEndpoint: 'https://api.openai.com/v1/chat/completions',
+      };
     },
-    async addMessage(sender, message, isUserMessage) {
-      // 메시지를 채팅 기록에 추가하는 메서드
-      this.chatHistory.unshift({ sender, message, isUserMessage })
-    },
-    async fetchAIResponse(prompt) {
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${this.apiKey}`,
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: prompt }],
-          temperature: 0.2,
-          max_tokens: 1000,
-          top_p: 1,
-          frequency_penalty: 0.5,
-          presence_penalty: 0.5,
-          stop: ["Human", "하지만"],
-        }),
-      }
+    methods: {
+      handleKeyDown(event) {
+        if (event.key === 'Enter') {
+          this.sendMessage();
+        }
+      },
+      async addMessage(sender, message, isUserMessage) {
+        this.chatHistory.unshift({ sender, message, isUserMessage });
+      },
+      async fetchAIResponse(prompt) {
+        const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.apiKey}`
+          },
+          body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: prompt }],
+            temperature: 0.2,
+            max_tokens: 1000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.5,
+            stop: ["Human", "하지만"],
+          }),
+        };
         
   
       try {
@@ -133,13 +99,16 @@ export default {
   
       if (message.length === 0) return
   
-      const isRecipeRelated = /레시피|요리법/.test(message) || /^[가-힣\s]+$/.test(message)
-  
-      if (isRecipeRelated) {
-        // this.addMessage('나', message, true);
-        this.userMessage = ''
 
-        let userQuery = message // 사용자의 검색어로 할당
+        const isRecipeRelated = /레시피|요리법/.test(message) || /^[가-힣\s]+$/.test(message);
+        
+        const memEmail = sessionStorage.getItem('memEmail'); 
+
+        if (isRecipeRelated) {
+          this.userMessage = '';
+
+          let userQuery = message;
+
 
   
         if (!/레시피|요리법/.test(message)) {
@@ -154,40 +123,63 @@ export default {
 
         this.addMessage('라따뚜AI', aiResponse, false)
   
-        // 숫자를 포함하는지 확인
-        if (aiResponse.match(/\d+/)) {
-        // 데이터를 Java 백엔드로 전송
-          try {
-            const dataToSend = {
+
+          
+          if (aiResponse.match(/\d+/)) {
+        try {
+          const dataToSend = {
               chat_title: userQuery,
               chat_content: aiResponse,
-            }
+              memEmail: memEmail,
+            };
 
-            const response = await axios.post('http://localhost:8888/chatinsert', dataToSend)
-
-            console.log('Data sent to Java backend:', response.data, dataToSend)
+            const response = await axios.post('http://localhost:8888/chatinsert', dataToSend);
+            console.log('백엔드로 데이터 전송 성공:', response.data, dataToSend);
           } catch (error) {
-            console.error('Error sending data to Java backend:', error)
+            console.error('백엔드로 데이터 전송 실패:', error);
+
           }
         }
-      } else {
-        this.addMessage('나', message, true)
-        this.addMessage('라따뚜AI', '죄송합니다. 레시피 관련 질문이나 음식 이름이 아닌 경우, 한글로 검색하지 않으신 경우엔 답변드리지 못합니다.', false)
-      }
-      this.userMessage = ''
+
+        this.userMessage = '';
+      },
+    },
+    mounted() {
+      this.addMessage('라따뚜AI', '안녕하세요! 어떤 레시피를 검색하고 싶으세요? 음식 이름만 말해주시면 조리법과 1인분 열량을 알려드리겠습니다!');
+      document.addEventListener('keydown', this.handleKeyDown);
+    },
+    beforeUnmount() {
+      document.removeEventListener('keydown', this.handleKeyDown);
+
     },
   },
 }
 </script>
     
     <style>
-  /* 스타일 영역에 CSS를 추가할 수 있습니다. */
   body {
     display: flex;
     align-items: center;
     justify-content: center;
     margin: 0;
     block-size: 100vh;
+  }
+
+  .message {
+    padding: 10px;
+    background-color: #e6e6e6;
+    border-block-start: 1px solid #ccc;
+    margin-block-start: 5px;
+  }
+
+  #chat-container {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #ccc;
+    background-image: url("../../assets/images/bg.jpeg");
+    background-position: center;
+    background-size: cover;
+    block-size: 700px;
   }
 
   .message {
@@ -240,22 +232,22 @@ export default {
   }
 
   .user-message {
-    align-self: flex-end; /* 사용자 메시지는 왼쪽으로 정렬 */
-    padding: 10px; /* 내용과 테두리 간격 */
-    border-radius: 15px; /* 말풍선을 둥글게 만듦 */
-    background: linear-gradient(to right, #fff176, #ffd700); /* 노란색 배경 */
+    align-self: flex-end;
+    padding: 10px;
+    border-radius: 15px;
+    background: linear-gradient(to right, #fff176, #ffd700);
     color: black;
-    margin-block-end: 10px; /* 아래 여백 추가 */
-    max-inline-size: 70%; /* 최대 너비 설정 */
+    margin-block-end: 10px;
+    max-inline-size: 70%;
   }
 
   .ai-message {
-    align-self: flex-start; /* AI 메시지는 오른쪽으로 정렬 */
-    padding: 10px; /* 내용과 테두리 간격 */
-    border-radius: 15px; /* 말풍선을 둥글게 만듦 */
-    background: linear-gradient(to right, #53a157, #32cd32); /* 초록색 배경 */
+    align-self: flex-start;
+    padding: 10px;
+    border-radius: 15px;
+    background: linear-gradient(to right, #53a157, #32cd32);
     color: white;
-    margin-block-end: 10px; /* 아래 여백 추가 */
-    max-inline-size: 70%; /* 최대 너비 설정 */
+    margin-block-end: 10px;
+    max-inline-size: 70%;
   }
     </style>
