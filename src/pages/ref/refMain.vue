97e8f99467910aa82fa235d99b55d870fa1fecd0
HongSC <hsc119119@naver.com>
2023-12-14 12:46:59 +0900

    성철 합침

--- src/pages/ref/refMain.vue
+++ src/pages/ref/refMain.vue
@@ -1,43 +1,14 @@
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      items: [],
-      headers: [
-        { text: '품목 이름', value: 'itemName' },
-        { text: '등록 날짜', value: 'registrationDate' },
-        { text: '남은 유통기한', value: 'expiryDate' },
-        { text: '수량', value: 'quantity' },
-      ],
-      userMail: sessionStorage.getItem('userMail'),
-    };
-  },
-  async created() {
-    try {
-      const response = await axios.post('/', { email: this.userMail });
-      this.items = response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
-};
-</script>
-
 <template>
-  <VDataTable
-    :headers="headers"
-    :items="items"
-    :items-per-page="10"
-    :sort-by="['itemName']"
-    :sort-desc="[false]"
-    class="elevation-1"
-  >
-    <template v-slot:item.name="{ item }">
-      <VChip color="blue" text-color="white">
-        {{ item.itemName }}
-      </VChip>
-    </template>
-  </VDataTable>
+ tmep
 </template>
+
+<script>
+export default {
+  name: 'refMain',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
