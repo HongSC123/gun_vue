40f0338a439c5913450a660ad5e3d6f1a68453ca
scgonzo21 <gonzo19924@gmail.com>
2023-12-15 17:10:59 +0900

    "아마도 최종"

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,24 @@
 <template>
+    <!-- <div class="large-image">
+      <img src="@/assets/images/maintitle.png" alt="Main Title">
+    </div> -->
   <div>
     <v-row>
-      <v-col
-        v-for="(image, index) in images"
-        :key="index"
-        cols="3"
-      >
+      <v-col v-for="(image, index) in images" :key="index" cols="3">
         <VCard
-          @mouseover="revealText(index)"
-          @mouseleave="hideText(index)"
+        @mouseenter="toggleImageAndText(index, true)"
+          @mouseleave="toggleImageAndText(index, false)"
           class="card-container"
         >
           <div class="image-container">
-            <img
-              :src="getImagePath(image)"
-              :alt="'Image ' + index + ' description'"
-              class="hover-image"
-            >
-            <span v-show="revealedIndex === index" class="overlay-text">
-              Overlay Text Here
-            </span>
+            <div class="image-wrapper">
+              <img :src="getImagePath(image)" :alt="'Image ' + index + ' description'" class="hover-image">
+              <div class="text-overlay" v-show="revealedIndex === index">
+                <span class="overlay-text">
+                  {{ texts[index] }}
+                </span>
+              </div>
+            </div>
           </div>
         </VCard>
       </v-col>
@@ -28,7 +27,7 @@
 </template>
 
 <script>
-import image3 from '@/assets/images/3.jpg';
+import image9 from '@/assets/images/9.jpg';
 import image6 from '@/assets/images/6.jpg';
 import image5 from '@/assets/images/5.jpg';
 import image7 from '@/assets/images/7.jpg';
@@ -36,17 +35,29 @@ import image7 from '@/assets/images/7.jpg';
 export default {
   data() {
     return {
-      images: [image3, image6, image5, image7],
-      revealedIndex: null
+      images: [image9, image6, image5, image7],
+      revealedIndex: null,
+      texts: [
+        "비만 여부 확인 및 AI를 통한 체형 분석",
+        "AI를 통한 체형분석과 식단/칼로리 관리",
+        "AI를 통한 사진의 식재료 분석, 냉장고 관리",
+        "챗GPT-챗봇으로 레시피 검색서비스 제공"
+      ]
     };
   },
   methods: {
-    revealText(index) {
+    toggleImageAndText(index, isHovering) {
+    const imgElement = document.querySelectorAll('.image-container img')[index];
+    if (isHovering) {
+      imgElement.style.filter = 'blur(3px)';
+      imgElement.style.transform = 'translateY(-100px)';
       this.revealedIndex = index;
-    },
-    hideText(index) {
+    } else {
+      imgElement.style.filter = 'none';
+      imgElement.style.transform = 'translateY(0)';
       this.revealedIndex = null;
-    },
+    }
+  },
     getImagePath(image) {
       return image;
     }
@@ -59,20 +70,20 @@ export default {
   cursor: pointer;
 }
 
-.image-container {
+.image-wrapper {
   position: relative;
   overflow: hidden;
-  transition: transform 0.3s;
 }
 
 .hover-image {
   display: block;
-  width: 100%;
-  height: auto;
-  transition: transform 0.3s;
+  width: auto; /* 이미지를 부모 요소에 맞게 조정 */
+  height: auto; /* 높이 자동 조정 */
+  object-fit: cover; /* 이미지를 확대 또는 축소하여 채움 */
+  transition: transform 0.3s ease;
 }
 
-.overlay-text {
+.text-overlay {
   position: absolute;
   bottom: 0;
   left: 0;
@@ -81,19 +92,11 @@ export default {
   color: white;
   padding: 10px;
   text-align: center;
-  transform: translateY(100%);
-  transition: transform 0.3s;
+  transition: opacity 0.3s ease-in-out;
+  opacity: 0;
 }
 
-.VCard:hover .image-container {
-  transform: translateY(-10px);
-}
-
-.VCard:hover .hover-image {
-  transform: translateY(10px);
-}
-
-.VCard:hover .overlay-text {
-  transform: translateY(0);
+.image-container:hover .text-overlay {
+  opacity: 1;
 }
 </style>
