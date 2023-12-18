967584b701fbc213cf2f5effb3ae73ecf7ebf2ed
JunWoo Choi <chlwnsdn78@gmail.com>
2023-12-18 10:24:36 +0900

    vue 마지막커밋

--- src/router/calorie.js
+++ src/router/calorie.js
@@ -1,16 +1,6 @@
-import calorieDetail from '@/pages/calorie/calenderDetail.vue'
+
 
 const calorie = [ 
-    {
-        heading: '칼로리',
-        path: '/pages/calorie',
-        children: [
-            {
-            path: '/calenderdetail',
-            name: 'calorieDetail',
-            component: calorieDetail,
-            },
-        ],
-    }
+    
 ]
 export default calorie
